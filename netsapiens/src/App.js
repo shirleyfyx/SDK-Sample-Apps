4bd123dab0b6a870d216ad514dd20f48e03238b6
Kiran Mandhane <k.mandhane@gmail.com>
2023-08-04 09:28:31 -0600

    changed netsapiens sample app

--- netsapiens/src/App.js
+++ netsapiens/src/App.js
@@ -1,3 +1,9 @@
+
+//There are two widgets: One for the team chat and one for drive/contacts
+//This means that the team chat widget is always running so there will be a nav event when you click a notif
+//However, this app does not work on many browsers for some reason
+//Also, if the team chat widget is on in the background on the room that you recieved a notif from, there will not be a nav event 
+
 import * as Callbridge from '@iotum/callbridge-js';
 import React, { useState, useRef, useEffect } from 'react';
 import styles from './submitForm.module.css';
@@ -7,15 +13,15 @@ function App() {
 const [token, setToken] = useState('');
 const [hostId, setHostId] = useState('');
 const [submitted, setSubmitted] = useState(false);
-const [showChatWidget, setShowChatWidget] = useState(false);
-const [showContacts, setShowContacts] = useState(false);
-const [showDrive, setShowDrive] = useState(false);
-
+const [yourApp, setYourApp] = useState(true); 
+const [unreadMessages, setUnreadMessages] = useState(); 
+const [start, setStart] = useState(); 
 
 // Create a reference for the widget container
 const container = useRef(null);
 // Store a reference to the current widget instance
-const widgetRef = useRef(null);
+const chatWidget  = useRef(null);
+const widget = useRef(null);
 
 
 const handleTokenChange = (event) => {
@@ -32,107 +38,108 @@ const handleSubmit = () => {
   setSubmitted(true);
 };
 
-
-const handleChatButtonClick = () => {
-  setShowChatWidget(true);
-  setShowDrive(false);
-  setShowContacts(false);
-};
-
-
-const handleDriveButtonClick = () => {
-  setShowChatWidget(false);
-  setShowDrive(true);
-  setShowContacts(false);
-};
-
-
-const handleContactsButtonClick = () => {
-  setShowChatWidget(false);
-  setShowDrive(false);
-  setShowContacts(true);
-};
-
-
-const destroyWidget = () => {
-  if (widgetRef.current) {
-    widgetRef.current.unload();
-    widgetRef.current = null;
-  }
-};
-
-
+//render two widgets 
 const renderWidget = () => {
-  destroyWidget(); // Destroy any existing widget before creating a new one
+  console.log("Two widgets were rendered: A chat room widget and a widget for the drive/contacts");
+  console.log("Both these widgets start as invisible");
 
-
-  if (showChatWidget) {
-    widgetRef.current = new Callbridge.Dashboard(
+  chatWidget.current = new Callbridge.Dashboard(
     {
-    domain: 'iotum.callbridge.rocks',
-    sso: {
-    token: token,
-    hostId: hostId,
+      domain: 'iotum.callbridge.rocks',
+      sso: {
+        token: token,
+        hostId: hostId,
     },
-    container: container.current,
-    },
-    'Team',
-    { layout: 'full' }
-    );
-  }
+      container: container.current,
+    }, 
+    "Team",
+  );
+    chatWidget.current.toggle(false);
 
+  chatWidget.current.on('dashboard.UNREAD_MESSAGES', (data) => {
+    console.log("There was an unread messages event")
+    const sum = Object.values(data.rooms).reduce((m, n) => m + n, 0);
+    setUnreadMessages(sum);
+  });
 
-  else if (showDrive) {
-    widgetRef.current = new Callbridge.Dashboard(
+  widget.current = new Callbridge.Dashboard(
     {
-    domain: 'iotum.callbridge.rocks',
-    sso: {
-    token: token,
-    hostId: hostId,
+      domain: 'iotum.callbridge.rocks',
+      sso: {
+        token: token,
+        hostId: hostId,
     },
-    container: container.current,
-    },
-    'Drive'
-    );
-  }
-
-
-  else if (showContacts) {
-    widgetRef.current = new Callbridge.Dashboard(
-    {
-    domain: 'iotum.callbridge.rocks',
-    sso: {
-    token: token,
-    hostId: hostId,
-    },
-    container: container.current,
-    },
-    'Contacts'
-    );
-  }
+      container: container.current,
+    }, 
+  );
+    widget.current.toggle(false);
 }
 
+useEffect(() => {
+  if(start === true) {
+    chatWidget.current.on('dashboard.NAVIGATE', (data) => {
+      if (data.pathname != '/') {
+        loadWidget('Team');
+        console.log("The chat widget navigated to this room: " + data.pathname);
+      }
+      else {
+        console.log("The chat widget navigated to an unspecified room");
+      }
+  });
+  }
+
+}, [start])
 
 useEffect(() => {
   if (submitted) {
     renderWidget();
   }
-// Cleanup function to remove the widget when the component unmounts
-  return () => {
-    destroyWidget();
-  };
-  }, [submitted, showChatWidget, showContacts, showDrive]);
+  }, [submitted]);
+
+const loadWidget = (service) => {
+  if(service === "") {
+    widget.current.toggle(false);
+    chatWidget.current.toggle(false);
+    chatWidget.current.load("Team", {layout: "full", pathname: "/"})
+    chatWidget.current.toggle(false);
+    setYourApp(true);
+    console.log("Load your app")
+  }
+  else if (service === "Team") {
+    setStart(true);
+    chatWidget.current.toggle(true);
+    widget.current.toggle(false);  
+    setYourApp(false); 
+    console.log("Load the team chat widget")
+  }  else {
+    setStart(true); 
+    widget.current.toggle(true); 
+    widget.current.load(service);
+    chatWidget.current.toggle(false);
+    chatWidget.current.load("Team", {layout: "full", pathname: "/"})
+    chatWidget.current.toggle(false);
+    console.log("Load the " + service +  " widget")
+    setYourApp(false); 
+  }
+}
+
 
 
   if (submitted) {
     return (
       <div className={styles.appContainer}>
-      <div className={styles.verticalTabContainer}>
-      <button onClick={handleChatButtonClick}>Team Chat</button>
-      <button onClick={handleDriveButtonClick}>Drive</button>
-      <button onClick={handleContactsButtonClick}>Contacts</button>
-      </div>
-      <div ref={container} className={styles.widgetContainer}></div>
+        <div className={styles.verticalTabContainer}>
+          <button onClick={() => loadWidget('')}>Your App</button>
+          <button onClick={() => loadWidget('Team')} style={{ position: 'relative' }}>
+          Team Chat
+            {unreadMessages > 0 && <span className={styles.badge}>{unreadMessages}</span>}
+          </button>
+          <button onClick={() => loadWidget('Drive')}>Drive</button>
+          <button onClick={() => loadWidget('Contacts')}>Contacts</button>
+        </div>
+        {yourApp && <div>Your app goes here</div>}
+
+        <div ref={container} className={styles.widgetContainer}></div>
       </div>
     );
   }
@@ -159,5 +166,3 @@ useEffect(() => {
 
 
 export default App;
-
-
