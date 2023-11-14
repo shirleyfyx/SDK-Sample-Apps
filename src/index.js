0d2e7657c2c878f652268c42e298681abd846177
Shirley Fang <fangyixuan0123@gmail.com>
2023-11-14 13:22:55 -0500

    Create a menu button to navigate to the list widget app

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import store from './components/ReduxStore.js';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/token-page/TokenPage.js"
 import Menu from "./pages/menu-page/MenuPage.js";
-import ChatRoomApp from "./pages/chat-room-list/App.js"
+import ChatRoomListApp from "./pages/chat-room-list/App.js";
+import ListWidgetUiApp from './pages/list-widget-ui/App.js';
 import './navigation/MenuPageButtons/MenuPageButtons.css';
 import './navigation/MenuPageButtons/ChatRoomListButton.js'
 
@@ -20,8 +21,12 @@ const router = createBrowserRouter([
   },
   {
     path: "/chat-room-list",
-    element: <ChatRoomApp />, 
+    element: <ChatRoomListApp />, 
   },
+  {
+    path: "/list-widget-ui",
+    element: <ListWidgetUiApp />,
+  }
 ]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
--- src/pages/menu-page/MenuPage.js
+++ src/pages/menu-page/MenuPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './MenuPage.css';
-import ChatRoomListButton from '../../navigation/MenuPageButtons/ChatRoomListButton.js'
+import ChatRoomListButton from '../../navigation/MenuPageButtons/ChatRoomListButton.js';
+import ListWidgetUiButton from '../../navigation/MenuPageButtons/ListWidgetUiButton.js';
 import TokenButton from '../../navigation/TokenButton/TokenButton.js';
 
 const Menu = () => {
@@ -8,6 +9,7 @@ const Menu = () => {
     <div className="menu-container">
       <h1>Iotum Sample Apps</h1>
       <ChatRoomListButton />
+      <ListWidgetUiButton />
       <TokenButton position="left"/>
     </div>
   );
