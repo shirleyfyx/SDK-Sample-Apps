d00ea621eea3635a02f67d369d50314adac160b7
Shirley Fang <fangyixuan0123@gmail.com>
2023-11-15 13:16:19 -0500

    Implement lazy loading with suspense fallback for multiple components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from "react-dom/client";
 import { Provider } from 'react-redux';
 import store from './components/ReduxStore.js';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./pages/token-page/TokenPage.js"
 import Menu from "./pages/menu-page/MenuPage.js";
-import ChatRoomListApp from "./pages/chat-room-list/App.js";
-import ListWidgetUiApp from './pages/list-widget-ui/App.js';
 import './navigation/MenuPageButtons/MenuPageButtons.css';
-import './navigation/MenuPageButtons/ChatRoomListButton.js'
+
+// Using React.lazy to dynamically import the components
+const ChatRoomListApp = React.lazy(() => import("./pages/chat-room-list/App.js"));
+const ListWidgetUiApp = React.lazy(() => import('./pages/list-widget-ui/App.js'));
 
 const router = createBrowserRouter([
   {
@@ -19,13 +20,22 @@ const router = createBrowserRouter([
     path: "/menu",
     element: <Menu />,
   },
+  // Wrap the component with Suspense and provide a fallback
   {
     path: "/chat-room-list",
-    element: <ChatRoomListApp />, 
+    element: (
+      <Suspense fallback={<div>Loading Chat Room List...</div>}>
+        <ChatRoomListApp />
+      </Suspense>
+    ),
   },
   {
     path: "/list-widget-ui",
-    element: <ListWidgetUiApp />,
+    element: (
+      <Suspense fallback={<div>Loading List Widget UI...</div>}>
+        <ListWidgetUiApp />
+      </Suspense>
+    ),
   }
 ]);
 
