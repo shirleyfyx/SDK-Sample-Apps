bcd7a7e050a0160be11e9b88de81218649631257
Shirley Fang <fangyixuan0123@gmail.com>
2023-11-16 11:21:05 -0500

    Change the credentials management to Redux for simple-meeting

--- src/pages/simple-meeting/App.js
+++ src/pages/simple-meeting/App.js
@@ -1,51 +1,34 @@
-import * as Callbridge from '@iotum/callbridge-js';
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect, useCallback } from 'react';
 import styles from './submitForm.module.css';
 import TokenButton from '../../navigation/TokenButton/TokenButton';
 import MenuButton from '../../navigation/MenuButton/MenuButton';
+import * as Callbridge from '@iotum/callbridge-js';
+import { useSelector } from 'react-redux';
 
-function App() {
-  const [token, setToken] = useState('');
-  const [hostId, setHostId] = useState('');
-  const [submitted, setSubmitted] = useState(false);
+const App = () => {
   const [accessCode, setAccessCode] = useState('');
-  const [domain, setDomain] = useState('iotum.callbridge.rocks'); // default domain
-
   const container = useRef(null);
   const widget = useRef(null);
 
-  const handleTokenChange = (event) => {
-    setToken(event.target.value);
-  };
-
-  const handleHostIdChange = (event) => {
-    setHostId(event.target.value);
-  };
-
-  const handleSubmit = () => {
-    setSubmitted(true);
-  };
+  const credentials = useSelector(state => state.credentials);
 
   const handleAccessCode = (event) => {
-    setAccessCode(event.target.value)
+    setAccessCode(event.target.value);
   }
 
-  const handleDomainChange = (event) => {
-    setDomain(event.target.value);
-  }
-
-  const renderWidget = () => {
-    console.log("render meeting widget")
+  const renderWidget = useCallback(() => {
+    console.log("render meeting widget");
 
     if (widget.current) {
       widget.current.unload();
     }
+
     widget.current = new Callbridge.Meeting(
       {
-        domain: domain, // Use the domain from state
+        domain: credentials.domain, 
         sso: {
-          token: token,
-          hostId: hostId
+          token: credentials.token,
+          hostId: credentials.hostId
         },
         container: container.current,
       },
@@ -53,53 +36,38 @@ function App() {
       {
         skipJoin: true
       }
-    )
-  }
-
-  if (submitted) {
-    return (
-      <div className={styles.appContainer}>
-        <div className={styles.startButton}>
-          <div className={styles.accessCodeLabel}>Access Code:</div>
-          <input
-            type="text"
-            className={styles.accessCodeInput}
-            value={accessCode}
-            onChange={handleAccessCode}
-          />
-          <button className={styles.startMeetingButton} onClick={() => renderWidget()}>Start Meeting</button>
-        </div>
-        <TokenButton position='right'/>
-        <MenuButton position="right"/>
-        <div ref={container} className={styles.widgetContainer}></div>  
-      </div>
-
     );
-  }
+  }, [credentials, accessCode]); // Dependencies: credentials and accessCode
+
+  useEffect(() => {
+    renderWidget();
+
+    // Cleanup function to unload the widget
+    return () => {
+      if (widget.current) {
+        widget.current.unload();
+        console.log("widget unloaded");
+      }
+    };
+  }, [renderWidget]); // Effect dependency: renderWidget
 
   return (
-    <div className="form-wrapper">
-      <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', fontSize: '18px', fontWeight: 'bold'}}>Simple Meeting App</div>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Domain:
-          <input type="text" value={domain} onChange={handleDomainChange} required/>
-        </label>
-        <br />
-        <label>
-          SSO Token:
-          <input type="text" value={token} onChange={handleTokenChange} required/>
-        </label>
-        <br />
-        <label>
-          Host ID:
-          <input type="text" value={hostId} onChange={handleHostIdChange} required/>
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+    <div className={styles.appContainer}>
+      <div className={styles.startButton}>
+        <div className={styles.accessCodeLabel}>Access Code:</div>
+        <input
+          type="text"
+          className={styles.accessCodeInput}
+          value={accessCode}
+          onChange={handleAccessCode}
+        />
+        <button className={styles.startMeetingButton} onClick={renderWidget}>Start Meeting</button>
+      </div>
+      <TokenButton position='right'/>
+      <MenuButton position="right"/>
+      <div ref={container} className={styles.widgetContainer}></div>  
     </div>
   );
-}
+};
 
 export default App;
