59ba8dc53f8d2305b9de6975f53154b92567497b
Shirley Fang <109609935+shirleyfyx@users.noreply.github.com>
2023-12-13 14:10:51 -0500

    Revert "Revert "Implement Guarded Route Feature for Enhanced Security""

--- src/components/ChatRoom/ChatRoom.js
+++ src/components/ChatRoom/ChatRoom.js
@@ -1,15 +1,20 @@
 import React, { useRef, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import * as Callbridge from '@iotum/callbridge-js';
 import styles from './chat.module.css';
 
 const ChatRoom = (props) => {
   const chatContainerRef = useRef(null); 
+  const credentials = useSelector(state => state.credentials)
 
   useEffect(() => {
+    // Use the domain from the Redux store, or a default value if not available
+    const domain = credentials.domain || 'iotum.callbridge.rocks'
+
     // This code will run after the component has rendered and the chat container is available in the DOM
     const chatRoom = new Callbridge.Dashboard(
       {
-        domain: 'iotum.callbridge.rocks',
+        domain: domain,
         container: chatContainerRef.current, // Use the DOM element reference
       },
       'Team',
@@ -23,7 +28,7 @@ const ChatRoom = (props) => {
       chatRoom.unload(); // Replace with actual cleanup method if available
       console.log("Chat room was closed: " + props.path);
     };
-  }, [props.path]); 
+  }, [props.path, credentials]); 
 
   // Render the chat container reference
   return <div ref={chatContainerRef} className={styles.chatRoom}></div>;
